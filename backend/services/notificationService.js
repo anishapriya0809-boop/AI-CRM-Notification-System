b1d2aa5b3aca5ee4c7939eb3a90b08211734fe4b
Shru457 <shrutikumari7861@gmail.com>
2026-07-07 23:48:27 +0530

    Added notification service

---
+++ backend/services/notificationService.js
@@ -0,0 +1,51 @@
+const Notification = require("../models/Notification");
+
+const createNotification = async (notificationData) => {
+  return await Notification.create(notificationData);
+};
+
+const getUnreadCount = async (tenantId, userId) => {
+  return await Notification.countDocuments({
+    tenantId,
+    read: false,
+    $or: [
+      { userId: null },
+      { userId: userId }
+    ]
+  });
+};
+
+const markNotificationRead = async (id, tenantId) => {
+  return await Notification.findOneAndUpdate(
+    { _id: id, tenantId },
+    {
+      read: true,
+      readAt: new Date()
+    },
+    { new: true }
+  );
+};
+
+const markAllNotificationsRead = async (tenantId, userId) => {
+  return await Notification.updateMany(
+    {
+      tenantId,
+      read: false,
+      $or: [
+        { userId: null },
+        { userId: userId }
+      ]
+    },
+    {
+      read: true,
+      readAt: new Date()
+    }
+  );
+};
+
+module.exports = {
+  createNotification,
+  getUnreadCount,
+  markNotificationRead,
+  markAllNotificationsRead,
+};
