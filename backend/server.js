8700fead072f76b677a2f0302c4301991ff06cb9
Shru457 <shrutikumari7861@gmail.com>
2026-07-08 11:33:57 +0530

    Connect notification and trigger routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
+const notificationRoutes = require('./routes/notificationRoutes');
+const triggerRoutes = require('./routes/triggerRoutes');
 
 dotenv.config();
 
@@ -11,6 +13,9 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/notifications", notificationRoutes);
+app.use("/demo", triggerRoutes);
 
 const PORT = process.env.PORT || 5005;
 
